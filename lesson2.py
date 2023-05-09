0c1d185c1c4389d136f9d7d93aaaad796a7d3aa7
KaroliinaaB <48429@edu.raseko.fi>
2023-05-09 10:33:52 +0300

    valmis

---
+++ lesson2.py
@@ -0,0 +1,37 @@
+# Lesson2.py 
+# @author tekija: (PR, KB)  
+# @since pvm: (versio pvm 9.5.2023)  
+# @version versio: (versionumero 1.0)  
+# muutos: ()  
+#*****************************************************/
+
+#tähän ratkaisukoodi
+
+# Tehtävä 1
+blocks.place(GRASS, pos(0, 0, 0))
+blocks.place(BRICKS, pos(0, 0, 0))
+
+
+
+# Tehtävä2
+blocks.place(REDSTONE_LAMP, pos(0, 6, 0))
+blocks.place(REDSTONE_LAMP, pos(0, 7, 0))
+blocks.place(REDSTONE_LAMP, pos(0, 4, 0))
+
+
+
+# Tehtävä3
+blocks.place(BRICKS, pos(0, 0, 1))
+blocks.place(BRICKS, pos(0, 1, 2))
+blocks.place(BRICKS, pos(0, 2, 3))
+
+
+
+# Tehtävä4
+blocks.place(PLANKS_OAK, pos(-1, -1, 0))
+blocks.place(PLANKS_OAK, pos(-2, -1, 0))
+blocks.place(PLANKS_OAK, pos(-3, -1, 0))
+blocks.place(PLANKS_OAK, pos(-4, -1, 0))
+blocks.place(PLANKS_OAK, pos(-5, -1, 0))
+
+
